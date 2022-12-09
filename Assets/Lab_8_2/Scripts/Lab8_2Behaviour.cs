cc26af973e4a4533f442793d7dabd585c4779e3e
Razrob <razrobs@yandex.ru>
2022-12-09 17:35:57 +0300

    fix

--- Assets/Lab_8_2/Scripts/Lab8_2Behaviour.cs
+++ Assets/Lab_8_2/Scripts/Lab8_2Behaviour.cs
@@ -25,31 +25,34 @@ public class Lab8_2Behaviour : PhysicBehaviourBase
         Vector3 origin = new Vector3(0f, 2.75f, 0f);
 
         List<Vector3> points = new List<Vector3>();
-        ThrowRay(origin, 1f, Quaternion.Euler(0f, 0f, angle) * Vector3.down, ref points);
+        bool active = false;
+        ThrowRay(origin, 1f, Quaternion.Euler(0f, 0f, angle) * Vector3.down, ref points, ref active);
+         if (active)
+            _finalPoint?.SetPointColor(Color.green);
+         else
+            _finalPoint?.SetPointColor(Color.red);
 
         Line.positionCount = points.Count;
         Line.SetPositions(points.ToArray());
     }
 
-    private void CheckPoint(Vector3 origin, Vector3 direction, bool applyIfNot = false)
+    private bool CheckPoint(Vector3 origin, Vector3 direction, bool applyIfNot = false)
     {
         if (_finalPoint is null)
             _finalPoint = GameObject.FindObjectOfType<Point>(true);
 
-        if (Physics.Raycast(origin, direction, out RaycastHit hit1, 500, 1 << 4))
-            _finalPoint?.SetPointColor(Color.green);
-        else if (applyIfNot)
-            _finalPoint?.SetPointColor(Color.red);
+        return (Physics.Raycast(origin, direction, out RaycastHit hit1, 500, 1 << 4));
     }
 
-    private void ThrowRay(Vector3 origin, float originRefractionFactor, Vector3 direction, ref List<Vector3> points)
+    private void ThrowRay(Vector3 origin, float originRefractionFactor, Vector3 direction, ref List<Vector3> points, ref bool pointContains)
     {
         direction = direction.SetZ(0f);
         origin = origin.SetZ(0f);
 
         points.Add(origin);
 
-        CheckPoint(origin, direction);
+        if (!pointContains)
+            pointContains = CheckPoint(origin, direction);
 
         if (Physics.Raycast(origin, direction, out RaycastHit hit, 500, 1)
             && hit.transform.TryGetComponent(out RefractionSurface surface))
@@ -64,7 +67,8 @@ public class Lab8_2Behaviour : PhysicBehaviourBase
                 Vector3 firstNextDirection =
                     Quaternion.Euler(0f, 0f, -internalNextAngle) * -hit.normal;
 
-                CheckPoint(firstNextOrigin + firstNextDirection * 100f, -firstNextDirection);
+                if (!pointContains)
+                    pointContains = CheckPoint(firstNextOrigin + firstNextDirection * 100f, -firstNextDirection);
 
                 RaycastHit reverseHit = Physics.RaycastAll(firstNextOrigin + firstNextDirection * 100f, -firstNextDirection)
                     .Find(h => h.transform == hit.transform);
@@ -74,7 +78,8 @@ public class Lab8_2Behaviour : PhysicBehaviourBase
                 _lastNormal = reverseHit.normal;
 
                 if (Physics.Raycast(outOrigin - reverseHit.normal * 0.05f, firstNextDirection, 0.1f))
-                    ThrowRay(outOrigin - reverseHit.normal * 0.05f, surface.RefractionFactor, firstNextDirection, ref points);
+                    ThrowRay(outOrigin - reverseHit.normal * 0.05f, surface.RefractionFactor, firstNextDirection, ref points, 
+                        ref pointContains);
                 else
                 {
                     float a1 = Vector3.SignedAngle(-firstNextDirection, -reverseHit.normal, Vector3.forward);
@@ -84,12 +89,12 @@ public class Lab8_2Behaviour : PhysicBehaviourBase
                         float a = CalculateNextAngle(a1, surface.RefractionFactor, 1f);
                         Vector3 d = Quaternion.Euler(0f, 0f, -a) * _lastNormal;
 
-                        ThrowRay(outOrigin - reverseHit.normal * 0.05f, 1f, d, ref points);
+                        ThrowRay(outOrigin - reverseHit.normal * 0.05f, 1f, d, ref points, ref pointContains);
                     }
                     else
                     {
                         ThrowRay(outOrigin - reverseHit.normal * 0.05f, 1f, 
-                            Vector3.Reflect(firstNextDirection, -reverseHit.normal), ref points);
+                            Vector3.Reflect(firstNextDirection, -reverseHit.normal), ref points, ref pointContains);
                     }
                 }
             }
@@ -98,7 +103,7 @@ public class Lab8_2Behaviour : PhysicBehaviourBase
                 points.Add(hit.point);
                 _lastNormal = hit.normal;
 
-                ThrowRay(hit.point - hit.normal * 0.02f, 1f, Vector3.Reflect(direction, hit.normal), ref points);
+                ThrowRay(hit.point - hit.normal * 0.02f, 1f, Vector3.Reflect(direction, hit.normal), ref points, ref pointContains);
             }
         }
         else
